52449cf75c1e231a3fb6b0a1852e43850670ae23
Julienraptor01 <julienraptor01@gmail.com>
2023-11-23 10:40:53 +0100

    reformat

--- src/main/FichierUtilisateur.cpp
+++ src/main/FichierUtilisateur.cpp
@@ -1,34 +1,34 @@
 #include "FichierUtilisateur.h"
 
-int estPresent(const char* nom)
+int estPresent(const char *nom)
 {
-  // TO DO
-  return 0;
+	// TO DO
+	return 0;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////
-int hash(const char* motDePasse)
+int hash(const char *motDePasse)
 {
-  // TO DO
-  return 0;
+	// TO DO
+	return 0;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////
-void ajouteUtilisateur(const char* nom, const char* motDePasse)
+void ajouteUtilisateur(const char *nom, const char *motDePasse)
 {
-  // TO DO
+	// TO DO
 }
 
 ////////////////////////////////////////////////////////////////////////////////////
-int verifieMotDePasse(int pos, const char* motDePasse)
+int verifieMotDePasse(int pos, const char *motDePasse)
 {
-  // TO DO
-  return 0;
+	// TO DO
+	return 0;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////
 int listeUtilisateurs(UTILISATEUR *vecteur) // le vecteur doit etre suffisamment grand
 {
-  // TO DO
-  return 0;
+	// TO DO
+	return 0;
 }
--- src/main/mywindow.h
+++ src/main/mywindow.h
@@ -4,34 +4,37 @@
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MyWindow; }
+namespace Ui
+{
+	class MyWindow;
+}
 QT_END_NAMESPACE
 
 class MyWindow : public QMainWindow
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    MyWindow(QWidget *parent = nullptr);
-    ~MyWindow();
+	MyWindow(QWidget *parent = nullptr);
+	~MyWindow();
 
-    void setNom(const char *Text);
-    const char *getNom();
-    void setMotDePasse(const char *Text);
-    const char *getMotDePasse();
-    int isNouveauChecked();
-    void setResultat(const char *Text);
-    void ajouteTupleTableUtilisateurs(const char *nom, int hash);
-    void videTableUtilisateurs();
+	void setNom(const char *Text);
+	const char *getNom();
+	void setMotDePasse(const char *Text);
+	const char *getMotDePasse();
+	int isNouveauChecked();
+	void setResultat(const char *Text);
+	void ajouteTupleTableUtilisateurs(const char *nom, int hash);
+	void videTableUtilisateurs();
 
 private slots:
-    void on_pushButtonLogin_clicked();
-    void on_pushButtonAfficheFichier_clicked();
+	void on_pushButtonLogin_clicked();
+	void on_pushButtonAfficheFichier_clicked();
 
 private:
-    Ui::MyWindow *ui;
+	Ui::MyWindow *ui;
 
-    char _nom[20];
-    char _motDePasse[20];
+	char _nom[20];
+	char _motDePasse[20];
 };
 #endif // MYWINDOW_H
